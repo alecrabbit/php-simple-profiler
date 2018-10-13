91499d8ed5021ec245f718947fd2c35cc9962366
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-10-13 23:23:50 +0300

    First

---
+++ src/Profiler.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: alec
+ * Date: 13.10.18
+ * Time: 23:20
+ */
+
+namespace AlecRabbit;
+
+
+class Profiler
+{
+    public function __construct()
+    {
+        $this->value = 5;
+    }
+    
+    public function __toString()
+    {
+        return (string)$this->value;
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User: alec
+ * Date: 12.10.18
+ * Time: 15:12
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Dumper\ServerDumper;
+
+$cloner = new VarCloner();
+$fallbackDumper = \in_array(\PHP_SAPI, array('cli', 'phpdbg')) ? new CliDumper() : new HtmlDumper();
+$dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper, array(
+    'cli' => new CliContextProvider(),
+    'source' => new SourceContextProvider(),
+));
+
+VarDumper::setHandler(function ($var) use ($cloner, $dumper) {
+    $dumper->dump($cloner->cloneVar($var));
+});
