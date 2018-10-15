5b34922a566ca1f53e92ed3c01f88e18068165ac
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-10-15 03:17:36 +0300

    mod

--- tests/Unit/ProfilerTest.php
+++ tests/Unit/ProfilerTest.php
@@ -39,14 +39,46 @@ class ProfilerTest extends TestCase
     public function TimerCreation()
     {
         $profiler = new Profiler();
+        $profiler->counter('new')->bumpUp();
         $profiler->timer('new')->start();
         $profiler->timer('new')->check();
-        $this->assertEquals(0.00000214, $profiler->timer('new')->elapsed(),'', 0.001);
+        $this->assertEquals(0.00000214, $profiler->timer('new')->elapsed(), '', 0.001);
         $this->assertStringMatchesFormat(
             '%s [%s, %s, %s]',
             $profiler
                 ->timer('new', 'vol', 'buy', 'tor')
                 ->getName()
         );
+    }
+
+
+    /** @test */
+    public function ProfilerReport()
+    {
+        $profiler = new Profiler();
+        $profiler->counter('new')->bumpUp();
+        $profiler->counter()->bump();
+        $profiler
+            ->timer('new')
+            ->forceStart()
+            ->check();
+        $profiler
+            ->timer()
+            ->forceStart()
+            ->check();
+        $this->assertEquals(1, $profiler->counter('new')->getValue());
+
+        $this->assertEquals(0.00000214, $profiler->timer('new')->elapsed(), '', 0.001);
+        $profiler
+            ->timer('new', 'vol', 'buy', 'tor')
+            ->forceStart()->check();
+        $report = $profiler->report();
+        $report_extended = $profiler->report(true);
+        $this->assertInternalType('array', $report);
+        $this->assertCount(5, $report);
+
+        $this->assertInternalType('array', $report_extended);
+        $this->assertCount(5, $report_extended);
+
     }
 }
