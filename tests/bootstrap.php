a569a02017f7b5e1105583182dc4bb161bff0919
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-11-01 16:26:05 +0200

    mod

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,13 +7,28 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-use Symfony\Component\VarDumper\VarDumper;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
 use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
 use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 use Symfony\Component\VarDumper\Dumper\ServerDumper;
+use Symfony\Component\VarDumper\VarDumper;
+
+// snippet
+if (!defined('APP_DEBUG')) {
+    define('APP_DEBUG', true);
+}
+
+// snippet
+if (defined('APP_DEBUG') && APP_DEBUG ) {
+    if (!defined('DEBUG_DUMP_EXCEPTION')) {
+        define('DEBUG_DUMP_EXCEPTION', false); // change to 'true' to dump exception message and trace
+    }
+    if (!defined('DEBUG_DUMP_EXCEPTION_CLASS')) {
+        define('DEBUG_DUMP_EXCEPTION_CLASS', false); // change to 'true' to dump exception class
+    }
+}
 
 $cloner = new VarCloner();
 $fallbackDumper = \in_array(\PHP_SAPI, array('cli', 'phpdbg')) ? new CliDumper() : new HtmlDumper();
