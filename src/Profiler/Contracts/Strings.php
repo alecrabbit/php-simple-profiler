ac8d24cc9a0d625e76674793911d7997078b4dee
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-11-03 22:19:57 +0200

    mod

--- src/Profiler/Contracts/Strings.php
+++ src/Profiler/Contracts/Strings.php
@@ -10,18 +10,18 @@ namespace AlecRabbit\Profiler\Contracts;
 
 interface Strings
 {
-    const _NAME = 'name';
-    const _AVG = 'avg';
-    const _MIN = 'min';
-    const _MAX = 'max';
-    const _COUNT = 'count';
-    const _LAST = 'last';
-    const _EXTENDED = 'extended';
+    public const _NAME = 'name';
+    public const _AVG = 'avg';
+    public const _MIN = 'min';
+    public const _MAX = 'max';
+    public const _COUNT = 'count';
+    public const _LAST = 'last';
+    public const _EXTENDED = 'extended';
 
-    const _COUNTERS = 'counters';
-    const _TIMERS = 'timers';
-    const _DEFAULT = 'default';
+    public const _COUNTERS = 'counters';
+    public const _TIMERS = 'timers';
+    public const _DEFAULT = 'default';
 
-    const _NAME_FORMAT = '%s [%s]';
+    public const _NAME_FORMAT = '%s [%s]';
 
 }
