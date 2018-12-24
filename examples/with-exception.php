772f18d8edea322e02565165db5b3960586b7498
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-12-24 18:04:37 +0200

    examples

--- examples/with-exception.php
+++ examples/with-exception.php
@@ -46,3 +46,24 @@ $benchmark
     ->color()
     ->run(true);
 echo $benchmark->elapsed() . PHP_EOL;
+
+// approx. output:
+//
+//Running benchmarks(50000):
+//....................
+//
+//Benchmark:
+//327.7ns (+0.0%) ⟨0⟩ Closure::__invoke(string) "fast function"
+//return: string "'a'"
+//18.2μs (+5,462.1%) ⟨1⟩ Closure::__invoke(integer) "slow function"
+//return: integer "1501"
+//Exceptions:
+//[⟨2⟩ Closure::__invoke]: [Exception]: Simulated exception
+//
+//Counter: 2(1)
+//Elapsed: 1s
+//Timer:[⟨0⟩ Closure::__invoke] Average: 327.7ns, Last: 0ns, Min(3): 0ns, Max(38577): 31μs, Count: 50000
+//Timer:[⟨1⟩ Closure::__invoke] Average: 18.2μs, Last: 18.1μs, Min(2): 16.9μs, Max(35010): 88.9μs, Count: 50000
+//Timer:[⟨2⟩ Closure::__invoke] Exception encountered
+//
+//Done in: 1031.053ms
