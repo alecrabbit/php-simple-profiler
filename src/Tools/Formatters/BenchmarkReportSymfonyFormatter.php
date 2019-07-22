7d2283fc9b55a94127c000b396e930d9697ef663
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-07-22 13:58:06 +0300

    init

--- src/Tools/Formatters/BenchmarkReportSymfonyFormatter.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace AlecRabbit\Tools\Formatters;
-
-class BenchmarkReportSymfonyFormatter extends BenchmarkReportFormatter
-{
-}
--- src/Tools/Formatters/Contracts/BenchmarkReportFormatterInterface.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace AlecRabbit\Tools\Formatters\Contracts;
-
-use AlecRabbit\Formatters\Contracts\FormatterInterface;
-
-interface BenchmarkReportFormatterInterface extends FormatterInterface
-{
-}
